9a96affba2c279bd77b656d9260ac71a2e9224e3
damballah <pougaud@gmail.com>
2019-03-16 10:00:15 +0100

    Update source.vb

--- source.vb
+++ source.vb
@@ -2,30 +2,33 @@ Module Module1
 
     Public Sub Main(ByVal Args() As String)
         On Error GoTo erreur
-        Dim chaine, ligne, fichier, contenu, resultat As String
-        Dim SR As New IO.StreamReader(Args(1))
+        Dim chaine, ligne, fichier, resultat As String
+        Dim i, n As Integer
 
         chaine = Args(0)
-        'fichier = Args(1)
-        resultat = "KO"
+        fichier = Args(1)
+        resultat = "Résultat de la recherche : KO"
+        i = 0
+        n = 0
 
+        FileOpen(1, fichier, OpenMode.Input)
+        Console.WriteLine("Recherche de la chaîne : '" + chaine + "'...")
 
-        Do Until SR.Peek = -1
-            ligne = ligne & SR.ReadLine()
-        Loop
+        While Not EOF(1)
+            ligne = LineInput(1)
+            i = i + 1
+            If ligne.Contains(chaine) = True Then
+                resultat = "Résultat de la recherche : OK --> chaîne trouvée à la ligne " + Str(i)
+                Exit While
+            Else
+            End If
+        End While
 
-        SR.Close()
-
-        If ligne.Contains(chaine) = True Then
-            resultat = "OK"
-        Else
-        End If
+        FileClose(1)
 
         Console.WriteLine(resultat)
 
         GoTo fin
-
-
 
 erreur:
         Console.WriteLine("Une erreur s'est produite, vérifiez que le fichier existe bien !")
